2d4be7e11fdc6e9871ae139c0ab786160b5eb2a6
Bryan <bryanwyk@gmail.com>
2021-04-04 18:50:33 +1000

    MainWrapper styling added

--- front-end/src/components/Wrapper/MainWrapper/MainWrapper.tsx
+++ front-end/src/components/Wrapper/MainWrapper/MainWrapper.tsx
@@ -3,6 +3,8 @@ import theme from '../../Theme/theme';
 
 const MainWrapper = styled.div`
 	padding-top: ${theme.height.navBarMobile};
+	max-width: 1440px;
+	margin: 0 auto;
 	@media (min-width: 768px) {
 		padding-top: ${theme.padding.mainWrapperDesktop};
 	}
